9809cb64bed7625985ba0f9053673a16bd2d8d88
Taker <taker_lp@hotmail.com>
2023-12-13 11:49:50 -0300

    test

---
+++ launcher.js
@@ -0,0 +1,6 @@
+// launcher.js
+document.getElementById('gameLauncher').addEventListener('click', function(event) {
+    // Abre el acceso directo al juego
+    event.preventDefault();
+    window.open('file:///E:/Torrent Descargas 8t/Hellboy.Web.of.Wyrd/game/Hellboy.exe', '_self');
+});
